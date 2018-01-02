14349bc5b052d0f6af9b11dc957229d86010dae0
chenzewei <chenzewei@zhujia360.com>
2018-01-02 15:28:16 +0800

    'update'

--- bin/zewail-react.js
+++ bin/zewail-react.js
@@ -4,6 +4,15 @@ const program = require('commander')
 const chalk = require('chalk')
 const package = require('../package.json')
 
+const newCli = require('../lib/new')
+
 program.version(package.version)
-    .option('new', 'new a zewail-react project')
-    .option('n', 'new a zewail-react project')
+  .option('new', 'new a zewail-react project')
+  .option('n', 'new a zewail-react project')
+  .parse(process.argv)
+
+
+
+if (program.new || program.n) {
+  newCli(process.argv)
+}
